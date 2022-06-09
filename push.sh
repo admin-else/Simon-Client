cb1c68ccb17cb4397a04d16aeffeffd6b196f331
simon <simon.kahre@gmail.com>
2022-06-09 14:38:56 +0200

    initial commit

--- push.sh
+++ push.sh
@@ -2,3 +2,5 @@ read -p "Enter commit message: " commitmessage
 git add .
 git commit -m"$commitmessage"
 git push https://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@github.com/admin-else/Simon-Client.git
+pause
+
